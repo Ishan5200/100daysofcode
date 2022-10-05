24b1f8797b84f75eaa650331ab3d1bba3d2e8549
Ishan Vats <ishanvats5200@gmail.com>
2022-10-06 01:35:20 +0530

    Updating all days

---
+++ Day_1/STOUO.java
@@ -0,0 +1,16 @@
+package Day_1;
+
+import java.util.Scanner;
+public class STOUO {
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            int x=sc.nextInt();
+         double y=sc.nextDouble();
+        sc.nextLine();
+        String s=sc.nextLine();
+          
+           
+         
+        System.out.println("String: "+s);  System.out.println("Double: "+y); System.out.println("Int: "+x);
+    }
+}
--- Day_1/Welcome.java
+++ Day_1/Welcome.java
@@ -1,8 +1,12 @@
 package Day_1;
 
 public class Welcome {
+    
 
-    public static void main(String[] args) {
-        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        public static void main(String[] args) {
+            /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+            System.out.println("Hello, World.\nHello, Java.");
+        
+        }
     }
-}
+
