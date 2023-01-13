f6f0d28c54ff412a4f8dc6ee9feb6dd1fc44d438
Ishan Vats <ishanvats5200@gmail.com>
2023-01-13 23:45:00 +0530

    Question

---
+++ Day_52/Q8.java
@@ -0,0 +1,37 @@
+package Day_52;
+
+/*Write a program to find the number of occurrences of a character in a string.
+
+Input format:
+Input to get a string in the first line and a character in the second line.
+
+Output format:
+Output the number of occurrences of a character in a string.
+
+Input: 
+utter
+t
+
+Output:
+2*/
+import java.util.Scanner;
+
+public class Q8 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter a string");
+            String s = sc.next();
+            char [] ch = s.toCharArray();
+            System.out.println("Enter a character");
+            char c = sc.next().charAt(0);
+            int ctr = 0;
+            for (int i = 0; i <s.length() ; i++) {
+                if (ch[i] == c)
+                    ctr++;
+            }
+            System.out.println("Number of occurrences of '"+c+"' in the string: "+ctr);
+        }
+    }
+}
+
+
