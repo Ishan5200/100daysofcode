c632597e67e6ab3a334b080541eb4579b6fb4c8d
Ishan Vats <ishanvats5200@gmail.com>
2023-01-19 23:32:57 +0530

    Ugly Number

---
+++ Day_58/UglyNumber.java
@@ -0,0 +1,28 @@
+package Day_58;
+
+
+
+/*Write a program to accept a number and check whether it is Ugly number or not*/
+import java.util.Scanner;
+public class UglyNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        int temp = n, k = 0;
+        while (n != 1) {
+            if (n % 2 == 0)
+                n = n / 2;
+            else if (n % 3 == 0)
+                n = n / 3;
+            else if (n % 5 == 0)
+                n = n / 5;
+            else {
+                System.out.println(temp + " is not an ugly number");
+                k = 1;
+                break;
+            }}
+            if (k == 0)
+                System.out.println(temp + " is an ugly number");
+    }
+}
