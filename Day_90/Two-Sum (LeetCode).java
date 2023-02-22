48e5131ae4687009ef40b9609e80ebbb9fd79ac5
Ishan Vats <98572923+Ishan5200@users.noreply.github.com>
2023-02-22 12:03:23 +0530

    Create Two-Sum (LeetCode).java

---
+++ Day_90/Two-Sum (LeetCode).java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        //int [] arr = {-1,-1};
+        for(int i=0; i<nums.length-1; i++){
+            for(int j=i+1; j<nums.length; j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                int [] arr = {i,j};
+                return arr;
+                }
+            }
+        }
+            int [] arr = {-1,-1};
+            return arr;
+        }
+    }
