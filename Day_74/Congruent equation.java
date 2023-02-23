42dad984ffc86dee6099657c643c9ffcf54b5d7e
Ishan Vats <98572923+Ishan5200@users.noreply.github.com>
2023-02-24 00:17:08 +0530

    Update Congruent equation.java

--- Day_74/Congruent equation.java
+++ Day_74/Congruent equation.java
@@ -1,7 +1,8 @@
 
 
 class CongruentEqu{
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
         int num[] = {3,4,5};
         int rem[] = {2,3,1};
         int x = 1 , k=rem.length;
